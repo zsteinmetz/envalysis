cde5391e0701bcb2c399756caf896cc468422bde
zsteinmetz <steinmetz@posteo.de>
2015-05-24 12:36:58 +0200

    envalysis init

---
+++ R/pkgname.R
@@ -0,0 +1,20 @@
+#' @title Miscellaneous functions for environmental analyses
+#'
+#' @description
+#' Data manipulation tools and functions that may be helpful for data analyses in
+#' environmental chemistry and ecotoxicology.
+#' 
+#' @details
+#' \tabular{ll}{
+#' Version: \tab 0.1\cr
+#' Licence: \tab GPLv3\cr
+#' }
+#'
+#' @author Zacharias Steinmetz (\email{stei4785@@uni-landau.de})
+#' 
+#' @references
+#' Borcard D, Gillet F, Legendre P. 2011. Numerical ecology with R. Springer, New York.
+#'
+#' @docType package
+#' @name envalysis
+NULL
