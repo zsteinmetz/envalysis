2c9488e2a13bff3449f8c0b05d97e3ed7f7780a3
Zacharias Steinmetz <steinmetz@posteo.de>
2015-09-02 18:37:36 +0200

    Improved signifig function
    signifig can now handle missing error values (NAs)

--- R/signifig.R
+++ R/signifig.R
@@ -5,24 +5,26 @@
 #' with its respective \code{error} term (e.g. confidence interval or standard deviation).
 #' 
 #' @usage
-#' signifig(mean, error, data, method = "pm")
+#' signifig(mean, error, data, round.na = 2, method = "pm")
 #'
 #' @param mean a numeric vector or data frame object containing the averaged values
 #' @param error a numeric vector or data frame object containing the respective error terms
 #' @param data a data frame containing the specified columns. If empty, \code{mean}
 #' and \code{error} need to be given as numeric vectors
+#' @param round.na an integer controlling how the mean value should be rounded
+#' when no error data was given
 #' @param method a string specifying the output method to be used. The default method
 #' \code{"pm"} reports the results as "3 ± 6", while \code{"par"} results in outputs
 #' like "401 (89)".
 #' 
 #' @examples
-#' signifig(mean = c(0.28,5), error = c(0.688, 8), method = "par")
+#' signifig(mean = c(0.28,5), error = c(0.688, 8), round.na = 2, method = "par")
 #' 
 #' @references
 #' Taylor JR. 1997. Error analysis: the study of uncertainties in physical measurements.
 #' University Science Books, Sausalito, CA.
 #' 
-signifig <- function(mean, error, data, method = "pm") {
+signifig <- function(mean, error, data, round.na = 2, method = "pm") {
   if (!missing(data)) {
     mean <- data[, deparse(substitute(mean))]
     error <- data[, deparse(substitute(error))]
@@ -36,8 +38,15 @@ signifig <- function(mean, error, data, method = "pm") {
   output <- c()
   for (i in 1:length(mean)) {
     e <- signif(error[i], 1)
-    if (e >= 1) { m <- signif(mean[i], 1)
-    } else { m <- round(mean[i], nchar(as.character(e))-2) }
+    if (is.na(e)) {
+      m <- round(mean[i], round.na)
+    } else {
+      if (e >= 1) {
+        m <- round(mean[i], -nchar(as.character(e))+1)
+      } else {
+        m <- round(mean[i], nchar(as.character(e))-2)
+      }
+    }
     if (method == "pm") output <- c(output, paste(m,"\u00b1", e))
     if (method == "par") output <- c(output, paste(m," (", e,")", sep=""))
   }
