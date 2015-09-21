8d67b35e68566aba046de7f59c87dc1929672adf
Zacharias Steinmetz <steinmetz@posteo.de>
2015-09-21 16:22:14 +0200

    Make panel.border black

--- R/theme_publish.R
+++ R/theme_publish.R
@@ -41,6 +41,7 @@ theme_publish <- function(base_size = 12, base_family = "") {
       legend.position =   "bottom",
       
       panel.background =  element_rect(fill = "white"), 
+      panel.border = element_rect(color = "black"),
       panel.margin =      grid::unit(0.4, "lines"),
       
       strip.background =  element_rect(fill="white"), 
