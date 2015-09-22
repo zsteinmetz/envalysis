f09c03fff5fd7de3301b3d92d270d53ecff29d83
Zacharias Steinmetz <steinmetz@posteo.de>
2015-09-22 20:49:38 +0200

    Simplified LOD function
    The concentration does not need to be passed twice anymore

--- R/lod.R
+++ R/lod.R
@@ -6,18 +6,18 @@
 #' (blank value) within a stated confidence level.
 #' 
 #' @usage
-#' lod(object, conc, alpha = 0.05)
+#' lod(object, alpha = 0.05)
 #'
 #' @param object a univariate model object of class \code{lm} with a model formula 
-#' \code{Signal ~ Conc} or \code{Signal ~ Conc - 1} containing the linear regression of the
-#' analyte concentrations with respect to the signal intensity recorded
-#' @param conc the column expression specifying the respective concentrations
+#' \code{Signal ~ Concentration} or \code{Signal ~ Concentraion - 1} containing
+#' the linear regression of the analyte concentrations with respect to the signal
+#' intensity recorded
 #' @param alpha the error tolerance for the detection limit (critical value)
 #'  
 #' @examples
 #' data(icp)
 #' Cal <- lm(Signal ~ Conc, data = icp)
-#' lod(Cal, Conc)
+#' lod(Cal)
 #'
 #' @references
 #' Currie, LA. 1997. Nomenclature in evaluation of analytical methods including detection
@@ -26,17 +26,19 @@
 #' @seealso
 #' \code{\link{loq}}
 #' 
-lod <- function(object, conc, alpha = 0.05) {
-  if (missing(conc) | missing(object)) stop("Linear model or input data missing")
-  if (class(object) != "lm") stop("Input object needs to be of class 'lm'")
+lod <- function(object, alpha = 0.05) {
+  if (missing(object) | class(object) != "lm") 
+    stop("Input object needs to be of class 'lm'")
   
-  name <- deparse(substitute(conc))
-  conc <- object$model[,name]
+  conc <- object$model[[2]]
   
-  n <- length(unique(conc))
-  m <- round(length(conc)/n)
-  s <- summary(object)$sigma/summary(object)[[4]][name,"Estimate"]
-  t <- qt(1-alpha/2, n-summary(object)$df[1])
+  n <- length(table(conc))
+  m <- unique(table(conc))
+  
+  if (length(m) != 1) stop("Measurement replicates of unequal size")
+  
+  s <- summary(object)$sigma/coef(object)[2]
+  t <- 2*qt(1-alpha, n-summary(object)$df[1])
   Q <- sum((conc - mean(conc))^2)/m
   
   return(s*t*sqrt((1/n)+(1/m)+((mean(conc))^2/Q)))
