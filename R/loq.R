0f5e70d6069eea50778580c7d6c48702f3ff50f4
Zacharias Steinmetz <steinmetz@posteo.de>
2015-09-23 12:20:23 +0200

    Simplified LOQ function and corrected some erros
    The concentration does not need to be passed twice anymore and Student's t is now implemented correctly

--- R/loq.R
+++ R/loq.R
@@ -6,19 +6,19 @@
 #' with confidence.
 #' 
 #' @usage
-#' loq(object, conc, alpha = 0.05, beta = 0.5)
+#' loq(object, alpha = 0.05, beta = 1/3)
 #'
 #' @param object a univariate model object of class \code{lm} with a model formula 
-#' \code{Signal ~ Conc} or \code{Signal ~ Conc - 1} containing the linear regression of the
-#' analyte concentrations with respect to the signal intensity recorded
-#' @param conc the column expression specifying the respective concentrations
+#' \code{Signal ~ Concentration} or \code{Signal ~ Concentration - 1} containing
+#' the linear regression of the analyte concentrations with respect to the signal
+#' intensity recorded
 #' @param alpha the error tolerance for the quantification limit (critical value)
 #' @param beta the error tolerance for a false negative
 #'  
 #' @examples
 #' data(icp)
 #' Cal <- lm(Signal ~ Conc, data = icp)
-#' loq(Cal, Conc)
+#' loq(Cal)
 #'
 #' @references
 #' Currie, LA. 1997. Nomenclature in evaluation of analytical methods including detection
@@ -27,28 +27,21 @@
 #' @seealso
 #' \code{\link{lod}}
 #' 
-loq <- function(object, conc, alpha = 0.05, beta = 0.5) {
-  if (missing(conc) | missing(object)) stop("Linear model or input data missing")
-  if (class(object) != "lm") stop("Input object needs to be of class 'lm'")
+loq <- function(object, alpha = 0.05, beta = 1/3) {
+  if (missing(object) | class(object) != "lm") 
+    stop("Input object needs to be of class 'lm'")
   
-  name <- deparse(substitute(conc))
-  conc <- object$model[,name]
+  conc <- object$model[[2]]
   
-  n <- length(unique(conc))
-  m <- round(length(conc)/n)
-  s <- summary(object)$sigma/summary(object)[[4]][name,"Estimate"]
-  t <- qt(1-alpha/2, n-summary(object)$df[1])
+  n <- length(table(conc))
+  m <- unique(table(conc))
+  k <- 1/beta
+  
+  if (length(m) != 1) stop("Measurement replicates of unequal size")
+  
+  s <- summary(object)$sigma/coef(object)[2]
+  t <- 2*qt(1-alpha, n-summary(object)$df[1])
   Q <- sum((conc - mean(conc))^2)/m
   
-  k <- 1/beta  
-  a <- 2*(k*s*t)^2*n*m*mean(conc)
-  b <- (k*s*t)^2*(Q*n+Q*m+m*n*mean(conc)^2)
-  c <- n*m*(Q-(k*s*t)^2)
-  
-  if (a+4*b*c < 0) {
-    return(NA)
-    warning("LOQ calculation failed. NAs generated")
-  } else {
-    return((-a+sqrt(a+4*b*c))/(2*c))
-  }
+  return(k*s*t*sqrt(1/m+1/n+(k*lod(object, alpha)-mean(conc))^2/Q))
 }
