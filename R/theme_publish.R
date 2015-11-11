54ebecff85bb3eb849bb41ef6df4d9e767810bd7
Zacharias Steinmetz <steinmetz@posteo.de>
2015-11-11 09:37:26 +0100

    Font sizes more consistent now

--- R/theme_publish.R
+++ R/theme_publish.R
@@ -27,22 +27,23 @@ theme_publish <- function(base_size = 12, base_family = "") {
   ggplot2::theme_bw(base_size = base_size, base_family = base_family) %+replace% 
     ggplot2::theme(
       axis.line =         element_blank(),
-      axis.text.x =       element_text(size = base_size * 0.8 , lineheight = 0.9,
+      axis.text.x =       element_text(size = base_size * 0.875 , lineheight = 0.9,
                                        colour = "black", vjust = 1),
-      axis.text.y =       element_text(size = base_size * 0.8, lineheight = 0.9,
+      axis.text.y =       element_text(size = base_size * 0.875, lineheight = 0.9,
                                        colour = "black", hjust = 1),
       axis.ticks =        element_line(colour = "black", size = 0.15),
       axis.title.x =      element_text(size = base_size, vjust = -0.2),
-      axis.title.y =      element_text(size = base_size, angle = 90, vjust = 1),
+      axis.title.y =      element_text(size = base_size, angle = 90, vjust = 1.2),
       axis.ticks.length = grid::unit(0.25, "lines"),
       axis.ticks.margin = grid::unit(0.2, "lines"),
       
       legend.key =        element_blank(),
       legend.key.size =   grid::unit(1.2, "lines"),
-      legend.text =       element_text(size = base_size * 0.8, family = "sans"),
-      legend.title =      element_text(size = base_size * 0.8, face = "bold",
+      legend.text =       element_text(size = base_size, family = "sans"),
+      legend.title =      element_text(size = base_size, face = "bold",
                                        hjust = 0),
       legend.position =   "bottom",
+      legend.box = "horizontal",
       
       panel.background =  element_rect(fill = NA, colour = "black", size = 0.1),
       panel.border =      element_blank(),
@@ -51,11 +52,11 @@ theme_publish <- function(base_size = 12, base_family = "") {
       panel.margin =      grid::unit(0.4, "lines"),
       
       strip.background =  element_rect(colour = "black", fill = NA, size = 0.1), 
-      strip.text.x =      element_text(colour = "black", size = base_size * 0.8),
-      strip.text.y =      element_text(colour = "black", size = base_size * 0.8,
+      strip.text.x =      element_text(colour = "black", size = base_size),
+      strip.text.y =      element_text(colour = "black", size = base_size,
                                        angle = -90),
       
-      plot.title =        element_text(size = base_size * 1.2),
+      plot.title =        element_text(size = base_size * 1.125),
       plot.margin =       grid::unit(c(1, 1, 0.5, 0.5), "lines")
     )
 }
