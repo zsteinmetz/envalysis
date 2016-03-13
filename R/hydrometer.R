3372d01dcd433ac330f1d96f7237045510279bd6
Zacharias Steinmetz <steinmetz@posteo.de>
2016-03-13 13:12:17 +0100

    Further code optimization
    - Comments
    - More functions

--- R/hydrometer.R
+++ R/hydrometer.R
@@ -1,5 +1,6 @@
 texture <- function(time, temp, reading, blank, data, conc = 50, Gs = 2.65,
                     hydrometer = "auto", plot = F) {
+  # Data preparation
   data <- na.omit(data)
   if (!missing(data)) {
     time <- data[, deparse(substitute(time))]
@@ -10,6 +11,7 @@ texture <- function(time, temp, reading, blank, data, conc = 50, Gs = 2.65,
   Gs <- round(Gs, 2)
   temp <- round(temp)
   
+  # Error handling
   if (hydrometer == "auto") {
     if (class(reading) == "integer" & all(reading %in% 0:60)) {
       hydrometer <- '152H'
@@ -27,7 +29,8 @@ texture <- function(time, temp, reading, blank, data, conc = 50, Gs = 2.65,
   if (hydrometer == '151H') {
     int <- 275.0161; sl <- 264.5161
     alpha <- 1}
-    
+  
+  # Calculation according to ASTM
   corr_val <- (reading - blank) + ct[as.character(temp)]
   eff_depth <- (int - sl * corr_val) + 1 / 2 * (14 - (67 / 27.8))
   diam <- k[as.character(temp),as.character(Gs)] * sqrt(eff_depth / time)
@@ -38,21 +41,35 @@ texture <- function(time, temp, reading, blank, data, conc = 50, Gs = 2.65,
   # Fit NLS model
   fit <- nls(perc.passing ~ SSlogis(particle.size, Asym, xmid, scal),
              data = dist)
-  fun <- function(x) SSlogis(x, coef(fit)[1], coef(fit)[2], coef(fit)[3])
-  if (plot) {
-    plot(perc.passing ~ particle.size, data = dist,
-         main = 'Particle size distribution',
-         xlab = "Particle size", ylab = "Percent passing", log = "x")
-    curve(fun, add = T)
+  
+  # Retrieve main texture classes
+  tex_classes <- function(x, obj) {
+    bounds <- sig(x, obj)
+    
+    matrix(
+      c(
+        bounds["estimate", 1],
+        bounds["estimate", 2] - bounds["estimate", 1],
+        1 - bounds["estimate", 2],
+        bounds["st.error", 1],
+        bounds["st.error", 2] +
+          bounds["st.error", 1],
+        bounds["st.error", 2]
+      ),
+      ncol = 3,
+      byrow = T,
+      dimnames = list(c("Estimate", "Std. Error"),
+                      c('Clay', 'Silt', 'Sand'))
+    )
   }
-  bounds <- fun(c(0.002, 0.063, 0.05))
-  din <- c(bounds[1], bounds[2] - bounds[1], 1 - bounds[2])
-  usda <- c(bounds[1], bounds[3] - bounds[1], 1 - bounds[3])
-  names(din) <- names(usda) <- c('Clay', 'Silt', 'Sand')
+  
+  din <- tex_classes(c(0.002, 0.063), fit)
+  usda <- tex_classes(c(0.002, 0.05), fit)             
   
   out <- list(dist = dist, din = din, usda = usda, model = fit,
               info = c(Hydrometer = hydrometer, Gs = Gs, Conc = conc))
-  class(out) <- 'texture'
+  class(out) <- c(class(out), 'texture')
+  if (plot) plot(out)
   return(out)
 }
 
@@ -65,15 +82,26 @@ print.texture <- function(obj, ...) {
   names(obj$dist) <- c('Particle size', 'Percent passing')
   print(obj$dist, digits = 3, row.names = F)
   cat(paste0('\n',
-             'Soil texture classes:\n'))
-  print(rbind(" DIN" = obj$din, " USDA" = obj$usda), digits = 3)
+             'Soil texture classes (DIN):\n'))
+  print(obj$din, digits = 3)
+  cat(paste0('\n',
+             'Soil texture classes (USDA):\n'))
+  print(obj$usda, digits = 3)
 }
 
 plot.texture <- function(obj, main = 'Particle size distribution',
                          xlab = "Particle size", ylab = "Percent passing", ...) {
   plot(perc.passing ~ particle.size, data = obj$dist,
        main = main, xlab = xlab, ylab = ylab, log = "x")
-  fun <- function(x) SSlogis(x, coef(obj$model)[1], coef(obj$model)[2],
-                             coef(obj$model)[3])
-  curve(fun, add = T)
+  curve(sig(x, obj$model)[1,], add = T)
+}
+
+# Sigmoid helper function
+sig <- function(x, obj) {
+  model_est <- summary(obj)$coefficients[,1]
+  model_err <- summary(obj)$coefficients[,2]
+  estimate <- SSlogis(x, model_est[1], model_est[2], model_est[3])
+  lwr <- SSlogis(x, model_est[1]+model_err[1], model_est[2]+model_err[2], model_est[3]+model_err[3])
+  upr <- SSlogis(x, model_est[1]-model_err[1], model_est[2]-model_err[2], model_est[3]-model_err[3])
+  rbind(estimate, st.error = abs(upr - lwr)/2)
 }
