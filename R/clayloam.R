e491f9640f5f3e1887313152d5de93a1a03f82de
Zacharias Steinmetz <steinmetz@posteo.de>
2016-03-16 20:07:28 +0100

    Add doc for data

---
+++ R/clayloam.R
@@ -0,0 +1,27 @@
+#' @name clayloam
+#' @title Hydrometer readings for a clay loam
+#' @description
+#' Data obtained with an 152H hydrometer in accordance with ASTM D422-63(2007)e2.
+#' @usage
+#' data(clayloam)
+#' @format
+#' A data frame containing 7 rows and 4 columns with information on:
+#' \tabular{ll}{
+#' Time: \tab the measurement time in minutes\cr
+#' Temperature: \tab the temperature of the soil suspension\cr
+#' Reading: \tab the hydrometer reading at the bottom of the meniscus\cr
+#' Blank: \tab a blank value obtained in 5 g/L sodium
+#' hexametaphosphate solution (composite correction)\cr
+#' }
+#' 
+#' @references
+#' ASTM D422-63(2007)e2. 2007. Standard Test Method for Particle-Size Analysis
+#' of Soils. ASTM International, West Conshohocken, PA. Available from
+#' \url{http://www.astm.org/Standards/D422.htm}
+#' 
+#' @seealso 
+#' \code{\link{texture}}
+#' 
+#' @docType data
+#' @keywords data
+NULL
