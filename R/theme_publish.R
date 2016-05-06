5f894711c6a414a0cccb2523b3b47dda4932fcc6
Zacharias Steinmetz <steinmetz@posteo.de>
2016-05-06 14:49:26 +0200

    Cleaner look for theme_publish
    Inspired by cowplot

--- R/theme_publish.R
+++ R/theme_publish.R
@@ -7,10 +7,12 @@
 #' for scientific publications,
 #'
 #' @usage
-#' theme_publish(base_size = 12, base_family = "")
+#' theme_publish(base_size = 12, base_family = "", line_size = 0.25, ...)
 #'
 #' @param base_size base font size
 #' @param base_family base font family
+#' @param line_size base line size for, e.g. for ticks and axes
+#' @param \dots further arguments to be passed to \code{theme_bw}
 #'
 #' @examples
 #' require(ggplot2)
@@ -24,34 +26,60 @@
 #'
 #' @import ggplot2
 #' @export
-theme_publish <- function(base_size = 12, base_family = '') {
-  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+theme_publish <- function(base_size = 12, base_family = "",
+                          line_size = 0.25, ...) {
+  half_line <- base_size/2
+  small_rel <- 0.875
+  small_size <- small_rel * base_size
+  
+  theme_bw(base_size = base_size, base_family = base_family, ...) %+replace%
     theme(
-      axis.line = element_blank(),
-      axis.text = element_text(colour = 'black', size = 0.875 * base_size,
-                               lineheight = 0.9),
-      axis.text.x = element_text(margin = margin(2.5,2.5,5,2.5,'pt')),
-      axis.text.y = element_text(margin = margin(2.5,2.5,2.5,5,'pt')),
-      axis.ticks = element_line(colour = 'black', size = 0.25),
-      axis.title = element_text(colour = 'black', size = base_size),
+      rect = element_rect(fill = "transparent", colour = NA, color = NA,
+                          size = 0, linetype = 0),
+      text = element_text(family = base_family, face = "plain",
+                          colour = "black", size = base_size, hjust = 0.5,
+                          vjust = 0.5, angle = 0, lineheight = 0.9,
+                          margin = ggplot2::margin(), debug = F),
+      
+      axis.text = element_text(size = small_size),
+      axis.text.x = element_text(margin = ggplot2::margin(t = small_size/4),
+                                 vjust = 1),
+      axis.text.y = element_text(margin = ggplot2::margin(r = small_size/4), 
+                                 hjust = 1),
+      axis.title.x = element_text(margin = ggplot2::margin(t = small_size,
+                                                           b = small_size/4)),
+      axis.title.y = element_text(angle = 90,
+                                  margin = ggplot2::margin(r = small_size,
+                                                           l = small_size/4)),
+      axis.ticks = element_line(colour = "black", size = line_size),
       axis.ticks.length = unit(0.25, 'lines'),
-
+      
+      axis.line.x = element_line(colour = "black", size = line_size), 
+      axis.line.y = element_line(colour = "black", size = line_size), 
+      
+      legend.margin = unit(0.1, "cm"),
       legend.key = element_blank(),
-      legend.key.size = unit(base_size, 'pt'),
+      legend.key.size = unit(1.5 * base_size, "pt"),
       legend.key.width = unit(1.5 * base_size, 'pt'),
-      legend.text = element_text(size = base_size),
-      legend.title = element_text(size = base_size, face = 'bold'),
+      legend.text = element_text(size = rel(small_rel)),
+      legend.title = element_text(size = rel(small_rel), face = 'bold'),
       legend.position = 'bottom',
       legend.box = 'horizontal',
-
-      panel.background = element_rect(fill = NA, colour = "black", size = 0.125),
-      panel.border = element_blank(),
-      panel.grid = element_blank(),
-      panel.margin = unit(0.5, 'lines'),
-
-      strip.background = element_rect(colour = 'black', fill = NA, size = 0.125),
-      strip.text = element_text(colour = 'black', size = base_size),
-      strip.text.x = element_text(margin = margin(4,0,4,0)),
-      strip.text.y = element_text(margin = margin(0,4,0,4), angle = -90)
+      
+      panel.background = element_blank(),
+      panel.border = element_blank(), 
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(), 
+      
+      strip.text = element_text(size = base_size),
+      strip.background = element_rect(fill = NA, colour = "black", size = 0.125),
+      strip.text.x = element_text(face = 'bold',
+                                  margin = ggplot2::margin(b = small_size/2)),
+      strip.text.y = element_text(angle = -90, face = 'bold',
+                                  margin = ggplot2::margin(l = small_size/2)),
+      
+      plot.background = element_blank(),
+      plot.title = element_text(face = "bold", size = 1.125 * base_size, 
+                                margin = ggplot2::margin(b = half_line))
     )
 }
