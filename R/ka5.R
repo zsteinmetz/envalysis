d785cedc146819df2df55f017073fb5f6a80e1de
Zacharias Steinmetz <steinmetz@posteo.de>
2016-05-06 14:48:40 +0200

    Add texture data according to Bodenkundliche Kartieranleitung

---
+++ R/ka5.R
@@ -0,0 +1,27 @@
+#' @name ka5
+#' @title KA5 soil texture classes
+#' @description
+#' Soil texture classes for texture triangles as provided by
+#' \emph{Bodenkundliche Kartieranleitung} (Sponagel et al., 2005)
+#' @usage
+#' data(ka5)
+#' @format
+#' A data frame containing 71 rows and 5 columns with information on:
+#' \tabular{ll}{
+#' Clay: \tab the clay fraction in percent\cr
+#' Silt: \tab the silt fraction in percent\cr
+#' Sand: \tab the sand fraction in percent\cr
+#' Group: \tab the respective soil texture group\cr
+#' Class: \tab the main soil texture class\cr
+#' }
+#' 
+#' @references
+#' Sponagel H, Grottenthaler W, Hartmann KJ, Hartwich R, Janetzko P, Joisten H,
+#' Kühn D, Sabel KJ, Traidl R. 2005. Bodenkundliche Kartieranleitung:
+#' mit 103 Tabellen und 31 Listen. In Ad-Hoc-Arbeitsgruppe Boden der Staatlichen
+#' Geologischen Dienste und der Bundesanstalt für Geowissenschaften und Rohstoffe
+#' and Bundesanstalt für Geowissenschaften und Rohstoffe, eds. Schweizerbart, Stuttgart.
+#' 
+#' @docType data
+#' @keywords data
+NULL
