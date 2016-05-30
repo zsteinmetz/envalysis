41199e67fd30c661eb7482c4c6fd8b7a927ac496
Zacharias Steinmetz <steinmetz@posteo.de>
2016-05-30 21:19:59 +0200

    Added DIN sample data

---
+++ R/din32645.R
@@ -0,0 +1,22 @@
+#' @name din32645
+#' @title Calibration data from DIN 32645
+#' 
+#' @description
+#' Sample data for the calibration of carbon in water.
+#' 
+#' @usage
+#' data(din32645)
+#' 
+#' @format
+#' A data frame containing 20 rows and 2 columns with information on:
+#' \tabular{ll}{
+#' Conc: \tab nominal concentration in mg/L\cr
+#' Area: \tab measurement signal\cr
+#' }
+#' 
+#' @seealso
+#' \code{\link{calibration}}
+#' 
+#' @docType data
+#' @keywords data
+NULL
