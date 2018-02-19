cea495b4cad1e8a6af0b48a79ae7444a1cab4457
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2018-02-19 14:00:02 +0100

    Change to new testthat synthax

--- tests/testthat/test-helper-functions.R
+++ tests/testthat/test-helper-functions.R
@@ -1,17 +1,15 @@
 context("Helper functions")
 
 test_that("Bisdom scaled WDPTs work as expected", {
-  expect_that(bisdom(c(2,6,20,3,385)), equals(c(1, 2, 2, 1, 3)))
-  expect_that(bisdom("a string"), gives_warning())
+  expect_equal(bisdom(c(2,6,20,3,385)), c(1, 2, 2, 1, 3))
+  expect_warning(bisdom("a string"))
   expect_true(is.na(bisdom(NA)))
   
-  bis1 <- bisdom(3600)
-  bis2 <- bisdom(4000)
-  expect_identical(bis1, bis2)
+  expect_identical(bisdom(3600), bisdom(4000))
 })
 
 test_that("Confidence intervals give proper results", {
-  expect_that(round(CI(1:5), 2), equals(1.39))
+  expect_equal(round(CI(1:5), 2), 1.39)
   expect_true(is.na(CI(NA)))
 })
 
@@ -24,3 +22,9 @@ test_that("Convert frequencies to raw data", {
   names(test) <- c("var", "freq")
   expect_identical(df, test)
 })
+
+test_that("Root mean square errors (RMSE) give proper results", {
+  expect_equal(round(rmse(c(0.12,0.59,NA), c(0.15,0.63,1.2)), 4), 0.0354)
+  expect_true(is.na(rmse(NA, NA)))
+})
+
