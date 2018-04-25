51761ccb31b5e5f1dbea588f78e553aa8ff83f5b
Zacharias Steinmetz <steinmetz@posteo.de>
2018-04-26 00:11:17 +0200

    Tests for `mselect()`

---
+++ tests/testthat/test-mselect.R
@@ -0,0 +1,11 @@
+context("mselect")
+
+library(drc)
+ryegrass_mod <- drm(rootl ~ conc, data = ryegrass, fct = LL.4())
+
+test_that("Both functions give the same output", {
+  expect_equal(
+    envalysis::mselect(ryegrass_mod, list(LL.3(), W2.4(), baro5())),
+    drc::mselect(ryegrass_mod, list(LL.3(), W2.4(), baro5()))
+  )
+})
