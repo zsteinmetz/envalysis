309240d0cb565f0244bc16fc191dd023a92bb64f
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2019-04-26 14:49:26 +0200

    Added wrapper function for standard errors

--- R/CI.R
+++ R/CI.R
@@ -1,9 +1,9 @@
 #' @title Confidence interval
 #' 
 #' @description
-#' This wrapper function computes the confidence interval of the values in \code{x}.
-#' If \code{na.rm} is \code{TRUE}, missing values are removed before the 
-#' computation proceeds.
+#' This wrapper function computes the standard error or the confidence interval
+#' of the values in \code{x}. If \code{na.rm} is \code{TRUE}, missing values are
+#' removed before the computation proceeds.
 #'
 #' @param x a numeric vector or an \R object which is coercible to one by
 #' \code{as.vector(x, "numeric")}.
@@ -14,10 +14,16 @@
 #' \code{\link[stats]{sd}} for the standard deviation.
 #' 
 #' @examples
+#' se(1:5)
 #' CI(1:5)
 #' 
 #' @importFrom stats qnorm sd
 #' @export
+se <- function(x, na.rm = FALSE) {
+  sd(x, na.rm = na.rm) / sqrt(length(x))
+}
+
+#' @export
 CI <- function(x, level = 0.95, na.rm = FALSE) {
-  qnorm(1 - ((1 - level) / 2)) * sd(x, na.rm = na.rm) / sqrt(length(x))
+  qnorm(1 - ((1 - level) / 2)) * se(x, na.rm = na.rm)
 }
