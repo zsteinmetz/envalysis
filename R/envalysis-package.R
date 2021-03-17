c515b46f109784701fbd48f895f38be3d76a028a
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-17 17:51:32 +0100

    usethis package doc

---
+++ R/envalysis-package.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+#' @references
+#' Almeida, A. M. D., Castel-Branco, M. M., & Falcao, A. C. (2002). Linear
+#' regression for calibration lines revisited: weighting schemes for
+#' bioanalytical methods. \emph{Journal of Chromatography B}, \bold{774}(2),
+#' 215-222. \doi{10.1016/S1570-0232(02)00244-1}.
+#' 
+#' ASTM D422-63 (2007). \emph{Standard Test Method for Particle-Size Analysis
+#' of Soils}. Technical standard. ASTM International, West Conshohocken, PA.
+#' Available from \url{http://www.astm.org/Standards/D422.htm}
+#' 
+#' Borcard D, Gillet F, & Legendre P. (2011). \emph{Numerical ecology with R}.
+#' Springer, New York.
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
